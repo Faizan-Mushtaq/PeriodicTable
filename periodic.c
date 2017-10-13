1ecf5b6d4d72dd89aa9d5940cfa27e24b6ba0bba
thehananbhat <32674552+thehananbhat@users.noreply.github.com>
2017-10-13 22:36:28 +0530

    Create periodic.c

---
+++ periodic.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+void main()
+{
+	system("clear");
+	printf("\t\t\t                          **********************MODERN PERIODIC TABLE***********************\n\n\n");
+	printf("\t\t\tH                                                                                                                               He\n");
+	printf("\t\t\tLi     Be                                                                                     B      C      N      O      F     Ne\n");
+	printf("\t\t\tNa     Mg                                                                                     Al     Si     P      S      Cl    Ar\n");
+	printf("\t\t\tK      Ca      Sc     Ti      V      Cr      Mn      Fe      Co      Ni      Cu      Zn       Ga     Ge     As     Se     Br    Kr\n");
+	printf("\t\t\tRb     Sr      Y      Zr      Nb     Mo      Tc      Ru      Rh      Pd      Ag      Cd       In     Sn     Sb     Te     I     Xe\n");
+	printf("\t\t\tCs     Ba      La     Hf      Ta     W       Re      Os      Ir      Pt      Au      Hg       Tl     Pb     Bi     Po     At    Rn\n");
+	printf("\t\t\tFr     Ra      Ac     Rf      Db     Sg      Bh      Hs      Mt      Ds      Rg      Cn      Uut     Fl    Uup     Lv    Uus   Uuo\n");
+	printf("\n\n\n");
+	printf("\t\t\t                      La      Ce     Pr      Nd      Pm      Sm      Eu      Gd      Tb       Dy     Ho     Er     Tm     Yb    Lu\n");
+	printf("\t\t\t                      Ac      Th     Pa      U       Np      Pu      Am      Cm      Bk       Cf     Es     Fm     Md     No    Lr\n");
+	printf("\n\n\n");
+	printf("\t\t\t                          **********************************************************************");
+	printf("\n\n\n\n\n");
+}
